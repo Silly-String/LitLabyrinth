7ba7536c87ba96763cb9038f6525a6372f454ace
janelleyan <157239836+janelleyan@users.noreply.github.com>
2024-03-27 19:38:36 -0400

    Add files via upload

--- project2_part1.py
+++ project2_part1.py
@@ -17,9 +17,99 @@ please consult our Course Syllabus.
 
 This file is Copyright (c) 2024 CSC111 Teaching Team
 """
+from __future__ import annotations
+from typing import Any
+
+
+class _Vertex:
+    """A vertex in a graph.
+
+    Instance Attributes:
+        - item: The data stored in this vertex.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+    """
+    item: Any
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        self.item = item
+        self.neighbours = neighbours
+
+
+class _Book(_Vertex):
+    """A vertex in a graph.
+
+    Instance Attributes:
+        - item: The data stored in this vertex.
+        - genre: The genre of the book
+        - author: The author of the book
+        - blurb: A short summary of the book
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+    """
+    item: str
+    genre: str
+    author: str
+    blurb: str
+    reviews: dict
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        super().__init__(item, neighbours)
+        self.item = item
+        self.neighbours = neighbours
+
+
+class _User(_Vertex):
+    """A vertex in a graph.
+
+    Instance Attributes:
+        - item: The data stored in this vertex.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+    """
+    item: str
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        super().__init__(item, neighbours)
+        self.item = item
+        self.neighbours = neighbours
+
+
+class Graph:
+    """A graph.
+
+    Representation Invariants:
+        - all(item == self._vertices[item].item for item in self._vertices)
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod(verbose=True)
 
     # When you are ready to check your work with python_ta, uncomment the following lines.
@@ -27,6 +117,7 @@ if __name__ == '__main__':
     # You can use "Run file in Python Console" to run PythonTA,
     # and then also test your methods manually in the console.
     import python_ta
+
     python_ta.check_all(config={
         'max-line-length': 120,
     })
