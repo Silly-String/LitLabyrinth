e3a5634133d731f43de877d5eb665ad2bb7890d3
janelleyan <157239836+janelleyan@users.noreply.github.com>
2024-03-27 18:51:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+"""CSC111 Project 2: Applications of trees and graphs
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains the code for Project 2. Please consult
+the project handout for instructions and details.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2024 CSC111 Teaching Team
+"""
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
+    # You can use "Run file in Python Console" to run PythonTA,
+    # and then also test your methods manually in the console.
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 120,
+    })
