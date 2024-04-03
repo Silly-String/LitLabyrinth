c4d43b05423afffe529e7c52e0367e0526f873ab
Sill <158129203+Silly-String@users.noreply.github.com>
2024-04-03 06:43:45 -0400

    Update project2_visualization.py
    Fixed PyTA errors

--- project2_visualization.py
+++ project2_visualization.py
@@ -122,25 +122,40 @@ def visualize_graph_clusters(graph: project2_part1.Graph, clusters: list[set],
         x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
         y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
 
-    trace3 = Scatter(x=x_edges,
-                     y=y_edges,
-                     mode='lines',
-                     name='edges',
-                     line={"color": LINE_COLOUR, "width": 1},
-                     hoverinfo='none'
-                     )
-    trace4 = Scatter(x=[pos[index][0] for index in graph_nx.nodes],
-                     y=[pos[j][1] for j in graph_nx.nodes],
-                     mode='markers',
-                     name='nodes',
-                     marker={"symbol": 'circle-dot', "size": 5, "color": colors,
-                             "line": {"color": VERTEX_BORDER_COLOUR, "width": 0.5}},
-                     text=list(graph_nx.nodes),
-                     hovertemplate='%{text}',
-                     hoverlabel={'namelength': 0}
-                     )
+    # trace3 = Scatter(x=x_edges,
+    #                  y=y_edges,
+    #                  mode='lines',
+    #                  name='edges',
+    #                  line={"color": LINE_COLOUR, "width": 1},
+    #                  hoverinfo='none'
+    #                  )
+    # trace4 = Scatter(x=[pos[index][0] for index in graph_nx.nodes],
+    #                  y=[pos[j][1] for j in graph_nx.nodes],
+    #                  mode='markers',
+    #                  name='nodes',
+    #                  marker={"symbol": 'circle-dot', "size": 5, "color": colors,
+    #                          "line": {"color": VERTEX_BORDER_COLOUR, "width": 0.5}},
+    #                  text=list(graph_nx.nodes),
+    #                  hovertemplate='%{text}',
+    #                  hoverlabel={'namelength': 0}
+    #                  )
 
-    fig = Figure(data=[trace3, trace4])
+    fig = Figure(data=[Scatter(x=x_edges,
+                               y=y_edges,
+                               mode='lines',
+                               name='edges',
+                               line={"color": LINE_COLOUR, "width": 1},
+                               hoverinfo='none'),
+                       Scatter(x=[pos[index][0] for index in graph_nx.nodes],
+                               y=[pos[j][1] for j in graph_nx.nodes],
+                               mode='markers',
+                               name='nodes',
+                               marker={"symbol": 'circle-dot', "size": 5, "color": colors,
+                                       "line": {"color": VERTEX_BORDER_COLOUR, "width": 0.5}},
+                               text=list(graph_nx.nodes),
+                               hovertemplate='%{text}',
+                               hoverlabel={'namelength': 0}
+                               )])
     fig.update_layout({'showlegend': False})
     fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
     fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
